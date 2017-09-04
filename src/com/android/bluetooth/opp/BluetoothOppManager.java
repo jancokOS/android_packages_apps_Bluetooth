cf5ac905915ccaaf9b220c78406404a9304d6210
bibahR <khabiburrokhman@gmail.com>
2017-09-04 02:19:44 +0000

    Fix merging

--- src/com/android/bluetooth/opp/BluetoothOppManager.java
+++ src/com/android/bluetooth/opp/BluetoothOppManager.java
@@ -250,7 +250,7 @@ public class BluetoothOppManager {
         if (V) Log.v(TAG, "Application data stored to SharedPreference! ");
     }
 
-    public void saveSendingFileInfo(String mimeType, String uriString, boolean isHandover)
+    public void saveSendingFileInfo(String mimeType, String uriString, boolean isHandover, boolean fromExternal)
             throws IllegalArgumentException {
         synchronized (BluetoothOppManager.this) {
             mMultipleFlag = false;
@@ -266,7 +266,7 @@ public class BluetoothOppManager {
         }
     }
 
-    public void saveSendingFileInfo(String mimeType, ArrayList<Uri> uris, boolean isHandover)
+    public void saveSendingFileInfo(String mimeType, ArrayList<Uri> uris, boolean isHandover, boolean fromExternal)
             throws IllegalArgumentException {
         synchronized (BluetoothOppManager.this) {
             mMultipleFlag = true;
